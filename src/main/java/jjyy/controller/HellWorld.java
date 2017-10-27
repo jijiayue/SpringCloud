ae8d56dc211f99024c453f97b97f2299ae770cc9
jijiayue@forevercj.com <jijiayue@forevercj.com>
2017-10-27 13:58:49 +0800

    1

---
+++ src/main/java/jjyy/controller/HellWorld.java
@@ -0,0 +1,17 @@
+package jjyy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sunwei0427 on 2017/10/27.
+ */
+@RestController
+public class HellWorld {
+    //访问/hello或者/hi任何一个地址，都会返回一样的结果
+    @RequestMapping(value = {"/hello","/hi"},method = RequestMethod.GET)
+    public String say(){
+        return "hi you!!!";
+    }
+}
