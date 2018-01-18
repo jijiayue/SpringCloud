607e8922a762253150892f98ee231a71ea7f0e74
jijiayue@forevercj.com <jijiayue@forevercj.com>
2018-01-18 18:05:44 +0800

    config server

---
+++ config_server/src/main/java/jjyy/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package jjyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by sunwei0427 on 2018/1/18.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
